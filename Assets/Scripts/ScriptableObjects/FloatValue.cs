d5d4309bca094fba9b55fe47975822691f6ffca0
jackx201 <zahuldmz@live.com>
2026-05-20 01:37:33 -0600

    Fixed Issue with health initializing empty

--- Assets/Scripts/ScriptableObjects/FloatValue.cs
+++ Assets/Scripts/ScriptableObjects/FloatValue.cs
@@ -1,13 +1,20 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using System;
 
 [CreateAssetMenu]
 [System.Serializable]
-public class FloatValue : ScriptableObject
+public class FloatValue : ScriptableObject, ISerializationCallbackReceiver
 {
     public float initialValue;
     public float RuntimeValue;
     internal float value;
+
+    public void OnAfterDeserialize()
+    {
+        RuntimeValue = initialValue;
+    }
+
+    public void OnBeforeSerialize() { }
 }
