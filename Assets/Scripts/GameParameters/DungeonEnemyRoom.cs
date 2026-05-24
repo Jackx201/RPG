4169337e249dba950011aae39f15bdca7444f183
jackx201 <zahuldmz@live.com>
2026-05-23 18:55:20 -0600

    Fixed Dungeon 1 Scene

--- Assets/Scripts/GameParameters/DungeonEnemyRoom.cs
+++ Assets/Scripts/GameParameters/DungeonEnemyRoom.cs
@@ -1,81 +1,81 @@
-﻿// using System.Collections;
-// using System.Collections.Generic;
-// using UnityEngine;
+﻿ using System.Collections;
+ using System.Collections.Generic;
+ using UnityEngine;
 
-// public class DungeonEnemyRoom : DungeonRoom
-// {
+ public class DungeonEnemyRoom : DungeonRoom
+ {
 
-//     public Door[] doors;
+     public Door[] doors;
 
-//     public void CheckEnemies()
-//     {
-//         for( int i = 0; i < enemies.Length; i++)
-//         {
-//             if(enemies[i].gameObject.activeInHierarchy && i < enemies.Length - 1)
-//             {
-//                 return;
-//             }
-//         }
-//         OpenDoors();
-//     }
+     public void CheckEnemies()
+     {
+         for( int i = 0; i < enemies.Length; i++)
+         {
+             if(enemies[i].gameObject.activeInHierarchy && i < enemies.Length - 1)
+             {
+                 return;
+             }
+         }
+         OpenDoors();
+     }
 
-//         public override void OnTriggerEnter2D(Collider2D other)
-//     {
-//         if(other.CompareTag("Player") && !other.isTrigger)
-//         {
-//             int enemiesquantity = enemies.Length;
-//             int potsquantity = pots.Length;
+         public override void OnTriggerEnter2D(Collider2D other)
+     {
+         if(other.CompareTag("Player") && !other.isTrigger)
+         {
+             int enemiesquantity = enemies.Length;
+             int potsquantity = pots.Length;
 
-//             for(int i=0; i< enemiesquantity; i++)
-//             {
-//                 ChangeActive(enemies[i], true);
-//             }
+             for(int i=0; i< enemiesquantity; i++)
+             {
+                 ChangeActive(enemies[i], true);
+             }
 
-//             for (int i=0; i < potsquantity; i++)
-//             {
-//                 ChangeActive(pots[i], true);
-//             }
-//                 CloseDoors();
-//                 virtualCamera.SetActive(true);
-//         }
-//     }
+             for (int i=0; i < potsquantity; i++)
+             {
+                 ChangeActive(pots[i], true);
+             }
+                 CloseDoors();
+                 virtualCamera.SetActive(true);
+         }
+     }
 
-//     public override void OnTriggerExit2D(Collider2D other)
-//     {
-//         if(other.CompareTag("Player") && !other.isTrigger)
-//         {
-//             int enemiesquantity = enemies.Length;
-//             int potsquantity = pots.Length;
+     public override void OnTriggerExit2D(Collider2D other)
+     {
+         if(other.CompareTag("Player") && !other.isTrigger)
+         {
+             int enemiesquantity = enemies.Length;
+             int potsquantity = pots.Length;
 
-//             for(int i=0; i< enemiesquantity; i++)
-//             {
-//                 ChangeActive(enemies[i], false);
-//             }
+             for(int i=0; i< enemiesquantity; i++)
+             {
+                 ChangeActive(enemies[i], false);
+             }
 
-//             for (int i=0; i < potsquantity; i++)
-//             {
-//                 ChangeActive(pots[i], false);
-//             }
-//         }
-//         virtualCamera.SetActive(false);
-//         //CloseDoors();
-//     }
+             for (int i=0; i < potsquantity; i++)
+             {
+                 ChangeActive(pots[i], false);
+             }
+         }
+         virtualCamera.SetActive(false);
+         CloseDoors();
+     }
 
-//     public void CloseDoors()
-//     {
-//         int NumberOfDoors = doors.Length;
-//         for(int i=0; i < NumberOfDoors; i++)
-//         {
-//             doors[i].CloseDoor();
-//         }
-//     }
+     public void CloseDoors()
+     {
+         int NumberOfDoors = doors.Length;
+         for(int i=0; i < NumberOfDoors; i++)
+         {
+             doors[i].CloseDoor();
+         }
+     }
 
-//         public void OpenDoors()
-//     {
-//         int NumberOfDoors = doors.Length;
-//         for(int i=0; i < NumberOfDoors; i++)
-//         {
-//             doors[i].OpenDoor();
-//         }
-//     }
-// }
+         public void OpenDoors()
+     {
+         int NumberOfDoors = doors.Length;
+         for(int i=0; i < NumberOfDoors; i++)
+         {
+             doors[i].OpenDoor();
+         }
+     }
+ }
