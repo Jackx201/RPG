817dc1b3a22a5a7856239a5c37e6bd6685b476d3
jackx201 <zahuldmz@live.com>
2026-05-29 01:13:03 -0600

    Fixed Context Notification

--- Assets/Scripts/Player_Scripts/ContextClue.cs
+++ Assets/Scripts/Player_Scripts/ContextClue.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -6,12 +6,22 @@ public class ContextClue : MonoBehaviour
 {
     [SerializeField] private SpriteRenderer mySprite;
     [SerializeField] private bool clueActive = false;
+    private Animator myAnimator;
 
+    private void Awake()
+    {
+        myAnimator = GetComponent<Animator>();
+    }
 
     public void ChangeClue()
     {
         clueActive = !clueActive;
         mySprite.enabled = clueActive;
+        
+        if (myAnimator != null)
+        {
+            myAnimator.enabled = clueActive;
+        }
     }
 
 
